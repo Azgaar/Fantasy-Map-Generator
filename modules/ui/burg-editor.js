a60b2eed9f73f6bded589df383f928b70c3c4240
Azgaar <maxganiev@yandex.ru>
2020-04-04 01:17:47 +0300

    v1.3.36

--- modules/ui/burg-editor.js
+++ modules/ui/burg-editor.js
@@ -292,7 +292,7 @@ function editBurg(id) {
     } else openMFCG();
 
     function openMFCG(seed) {
-      if (!seed) {openURL(burg.MFCGlink); return;}
+      if (!seed && burg.MFCGlink) {openURL(burg.MFCGlink); return;}
       const name = elSelected.text();
       const size = Math.max(Math.min(rn(burg.population), 65), 6);
   
