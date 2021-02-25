137cbbf0b9cf765a584b7f1f076d64d66fdb688e
Azgaar <maxganiev@yandex.ru>
2021-02-25 15:07:09 +0300

    v1.5.61 - not fire onbeforeunload in electron

--- modules/ui/general.js
+++ modules/ui/general.js
@@ -9,7 +9,7 @@ $(window).resize(function(e) {
   changeMapSize();
 });
 
-window.onbeforeunload = () => "Are you sure you want to navigate away?";
+if (!navigator.userAgent.includes("Electron")) window.onbeforeunload = () => "Are you sure you want to navigate away?";
 
 // Tooltips
 const tooltip = document.getElementById("tooltip");
