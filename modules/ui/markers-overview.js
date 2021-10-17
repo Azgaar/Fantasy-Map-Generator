b0e788b57528b08fd5be681a1639039cd1dcd142
Azgaar <maxganiev@yandex.ru>
2021-10-17 14:03:59 +0300

    markers-overview - ability to invert lock and pinned statuses

--- modules/ui/markers-overview.js
+++ modules/ui/markers-overview.js
@@ -6,6 +6,8 @@ function overviewMarkers() {
 
   const markerGroup = document.getElementById("markers");
   const body = document.getElementById("markersBody");
+  const markersInverPin = document.getElementById("markersInverPin");
+  const markersInverLock = document.getElementById("markersInverLock");
   const markersFooterNumber = document.getElementById("markersFooterNumber");
   const markersOverviewRefresh = document.getElementById("markersOverviewRefresh");
   const markersAddFromOverview = document.getElementById("markersAddFromOverview");
@@ -25,6 +27,8 @@ function overviewMarkers() {
 
   const listeners = [
     listen(body, "click", handleLineClick),
+    listen(markersInverPin, "click", invertPin),
+    listen(markersInverLock, "click", invertLock),
     listen(markersOverviewRefresh, "click", addLines),
     listen(markersAddFromOverview, "click", toggleAddMarker),
     listen(markersGenerationConfig, "click", configMarkersGeneration),
@@ -45,12 +49,12 @@ function overviewMarkers() {
 
   function addLines() {
     const lines = pack.markers
-      .map(({i, type, icon, lock}) => {
+      .map(({i, type, icon, pinned, lock}) => {
         return `<div class="states" data-i=${i} data-type="${type}">
         <div data-tip="Marker icon and type" style="width:12em">${icon} ${type}</div>
         <span style="padding-right:.1em" data-tip="Edit marker" class="icon-pencil"></span>
         <span style="padding-right:.1em" data-tip="Focus on marker position" class="icon-dot-circled pointer"></span>
-        <span style="padding-right:.1em" data-tip="Pin marker (display only pinned markers)" class="icon-pin inactive pointer"></span>
+        <span style="padding-right:.1em" data-tip="Pin marker (display only pinned markers)" class="icon-pin ${pinned ? "" : "inactive"}" pointer"></span>
         <span style="padding-right:.1em" class="locks pointer ${lock ? "icon-lock" : "icon-lock-open inactive"}" onmouseover="showElementLockTip(event)"></span>
         <span data-tip="Remove marker" class="icon-trash-empty"></span>
       </div>`;
@@ -61,6 +65,27 @@ function overviewMarkers() {
     markersFooterNumber.innerText = pack.markers.length;
 
     applySorting(markersHeader);
+  }
+
+  function invertPin() {
+    let anyPinned = false;
+
+    pack.markers.forEach(marker => {
+      const pinned = !marker.pinned;
+      if (pinned) {
+        marker.pinned = true;
+        anyPinned = true;
+      } else delete marker.pinned;
+    });
+
+    markerGroup.setAttribute("pinned", anyPinned ? 1 : null);
+    drawMarkers();
+    addLines();
+  }
+
+  function invertLock() {
+    pack.markers = pack.markers.map(marker => ({...marker, lock: !marker.lock}));
+    addLines();
   }
 
   function openEditor(i) {
