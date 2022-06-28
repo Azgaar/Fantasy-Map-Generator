acf18d7324d4f10fda3328a3dc8d6f77ecf92700
Azgaar <maxganiev@yandex.ru>
2022-06-29 02:15:24 +0300

    fix: cultures upload cache

--- versioning.js
+++ versioning.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // version and caching control
-const version = "1.87.06"; // generator version, update each time
+const version = "1.87.07"; // generator version, update each time
 
 {
   document.title += " v" + version;
