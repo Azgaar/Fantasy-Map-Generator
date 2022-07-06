5e6f5993484fa3c5b0081c0b344f2682b98db305
Azgaar <maxganiev@yandex.ru>
2022-07-06 22:17:57 +0300

    refactor: move assets

--- src/scripts/listeners.ts
+++ src/scripts/listeners.ts
@@ -25,7 +25,7 @@ export function addGlobalListeners() {
 function registerServiceWorker() {
   "serviceWorker" in navigator &&
     window.addEventListener("load", () => {
-      navigator.serviceWorker.register("../../sw.js").catch(err => {
+      navigator.serviceWorker.register("../sw.js").catch(err => {
         console.error("ServiceWorker registration failed: ", err);
       });
     });
