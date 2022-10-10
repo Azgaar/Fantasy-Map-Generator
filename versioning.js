7945390b2cdbf2635f6c6b1763022b39ade599a9
Azgaar <maxganiev@yandex.com>
2022-10-10 22:48:52 +0300

    feat: pump version to 1.87.14, new cultures correction

--- versioning.js
+++ versioning.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // version and caching control
-const version = "1.87.13"; // generator version, update each time
+const version = "1.87.14"; // generator version, update each time
 
 {
   document.title += " v" + version;
