44b486c1011e41156cfdb4d9c9f6f81339c4fe94
Azgaar <maxganiev@yandex.com>
2023-05-12 22:22:26 +0400

    fix(#943): don't convert ocean pattern image to base64 if pattern is toggled off

--- versioning.js
+++ versioning.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // version and caching control
-const version = "1.89.16"; // generator version, update each time
+const version = "1.89.17"; // generator version, update each time
 
 {
   document.title += " v" + version;
