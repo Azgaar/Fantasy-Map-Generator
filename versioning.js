1c7c0f5a93c030b8e31be722fd6774858c4608f8
Efruz Yıldırır <30903352+yldrefruz@users.noreply.github.com>
2023-07-13 23:05:23 +0300

    Versioning fixed.

--- versioning.js
+++ versioning.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // version and caching control
-const version = "1.90.0"; // generator version, update each time
+const version = "1.89.33"; // generator version, update each time
 
 {
   document.title += " v" + version;
