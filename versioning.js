8817813de334701d3d020ccd3d5e6c6073403694
Azgaar <maxganiev@yandex.com>
2023-08-08 01:39:52 +0400

    fix: #974

--- versioning.js
+++ versioning.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // version and caching control
-const version = "1.90.02"; // generator version, update each time
+const version = "1.90.03"; // generator version, update each time
 
 {
   document.title += " v" + version;
