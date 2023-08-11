95503ebf38aeb4d8b6ef3415d8308262c3d419e6
Azgaar <maxganiev@yandex.com>
2023-08-11 19:03:21 +0400

    hotfix: filename conflict

--- versioning.js
+++ versioning.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // version and caching control
-const version = "1.92.00"; // generator version, update each time
+const version = "1.92.01"; // generator version, update each time
 
 {
   document.title += " v" + version;
