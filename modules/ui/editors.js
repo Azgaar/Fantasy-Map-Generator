52e30887633600f33ae33f12d815229c224763d1
Azgaar <azgaar.fmg@yandex.com>
2023-12-05 02:45:55 +0400

    fix: cultures editor - don't overwrite border color on color change

--- modules/ui/editors.js
+++ modules/ui/editors.js
@@ -1182,7 +1182,7 @@ async function editStates() {
 
 async function editCultures() {
   if (customization) return;
-  const Editor = await import("../dynamic/editors/cultures-editor.js?v=1.91.00");
+  const Editor = await import("../dynamic/editors/cultures-editor.js?v=1.95.04");
   Editor.open();
 }
 
