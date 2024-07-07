e4f2de6472f46025f24f90e764c9c29f1e188a1e
Azgaar <azgaar.fmg@yandex.com>
2024-07-07 17:17:19 +0200

    feat: configure world - checkbox to auto-apply changes (v1.97.16)

--- versioning.js
+++ versioning.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // version and caching control
-const version = "1.97.15"; // generator version, update each time
+const version = "1.97.16"; // generator version, update each time
 
 {
   document.title += " v" + version;
