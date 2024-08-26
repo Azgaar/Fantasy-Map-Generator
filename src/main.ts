185740b5fa88ace2e405cabafaa1ddf87b3e4f9a
kruschen <leonard.krusch@gmx.de>
2024-08-26 22:35:55 +0000

    Revert "jquery type imports"
    This reverts commit f90652b68c3d333e3aae1345b8f259d95cfad537.

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@
 // https://github.com/Azgaar/Fantasy-Map-Generator
 
 import "./components";
-import "jquery";
-import "jqueryui";
 // @ts-expect-error js-module
 import {defineSvg} from "./modules/define-svg";
 // @ts-expect-error js-module
