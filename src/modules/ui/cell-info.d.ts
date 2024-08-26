cdb8d29e62320fec777dc0f3ec3a9807dbcffe25
kruschen <leonard.krusch@gmx.de>
2024-08-26 21:02:59 +0000

    onhover resolved most types, few are temporary hacks marked with MARKER

---
+++ src/modules/ui/cell-info.d.ts
@@ -0,0 +1,5 @@
+// src/modules/ui/cell-info.d.ts
+declare module 'modules/ui/cell-info.js' {
+    export function updateCellInfo(coords: [number, number], cellId: number, gridCell: any): void;
+    // Add other exports as needed
+  }
