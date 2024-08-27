7ceeaa071fef77083e5f7d9136431b8ebb06b703
kruschen <leonard.krusch@gmx.de>
2024-08-27 16:32:19 +0000

    js files temporarly ignored for import, deleted leftover mapping

--- src/layers/renderers/index.ts
+++ src/layers/renderers/index.ts
@@ -3,22 +3,31 @@ import {drawBiomes} from "./drawBiomes";
 import {drawBorders} from "./drawBorders";
 import {drawBurgs} from "./drawBurgs";
 import {drawCells} from "./drawCells";
+// @ts-expect-error js-module
 import {drawCoordinates} from "./drawCoordinates.js"; //MARKER: drawCoordinates.js
 import {drawCultures} from "./drawCultures";
+// @ts-expect-error js-module
 import {drawEmblems} from "./drawEmblems.js"; //MARKER: drawEmblems.js
 import {drawFeatures} from "./drawFeatures";
+// @ts-expect-error js-module
 import {drawGrid} from "./drawGrid.js"; //MARKER: drawGrid.js
+// @ts-expect-error js-module
 import {drawHeightmap} from "./drawHeightmap.js";
+// @ts-expect-error js-module
 import {drawIce} from "./drawIce.js"; //MARKER: drawIce.js
 import {drawLabels} from "./drawLabels";
+// @ts-expect-error js-module
 import {drawMarkers} from "./drawMarkers.js"; //MARKER: drawMarkers.js
+// @ts-expect-error js-module
 import {drawPopulation} from "./drawPopulation.js"; //MARKER: drawPopulation.js
+// @ts-expect-error js-module
 import {drawPrecipitation} from "./drawPrecipitation.js"; //MARKER: drawPrecipitation.js
 import {drawProvinces} from "./drawProvinces";
 import {drawReligions} from "./drawReligions";
 import {drawRivers} from "./drawRivers";
 import {drawRoutes} from "./drawRoutes";
 import {drawStates} from "./drawStates";
+// @ts-expect-error js-module
 import {drawTemperature} from "./drawTemperature.js"; //MARKER: drawTemperature.js
 
 // Note: missed renderers are in toggle functions
--- src/modules/legend.d.ts
+++
@@ -1,6 +0,0 @@
-// src/modules/legend.d.ts
-declare module 'modules/legend.js' {
-    export function clearLegend(): void;
-    export function dragLegendBox(): void;
-    // Add other exports as needed
-  }
