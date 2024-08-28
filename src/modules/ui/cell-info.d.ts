2dd6935135132f0aec9246747eb6ee6ea0299c4d
kruschen <leonard.krusch@gmx.de>
2024-08-29 01:19:03 +0200

    Delete src/modules/ui/cell-info.d.ts
    delete cell-info.d.ts in favor of temporary js import

--- src/modules/ui/cell-info.d.ts
+++
@@ -1,5 +0,0 @@
-// src/modules/ui/cell-info.d.ts
-declare module 'modules/ui/cell-info.js' {
-    export function updateCellInfo(coords: [number, number], cellId: number, gridCell: any): void;
-    // Add other exports as needed
-  }
