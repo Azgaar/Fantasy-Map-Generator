2ebc2e97335cdf9a4876b6f35af4a4ff79a51b53
Azgaar <azgaar.fmg@yandex.com>
2024-09-06 00:03:15 +0200

    refactor: pump version to 1.104.00

--- modules/io/load.js
+++ modules/io/load.js
@@ -462,7 +462,7 @@ async function parseLoadedData(data, mapVersion) {
 
     {
       // dynamically import and run auto-update script
-      const {resolveVersionConflicts} = await import("../dynamic/auto-update.js?v=1.100.00");
+      const {resolveVersionConflicts} = await import("../dynamic/auto-update.js?v=1.104.00");
       resolveVersionConflicts(mapVersion);
     }
 
