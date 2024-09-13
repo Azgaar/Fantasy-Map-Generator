1571a5fd8e331f1bbee19fe90e341978227d5a34
Azgaar <azgaar.fmg@yandex.com>
2024-09-13 15:44:18 +0200

    refactor: improve tooltip logic for burg labels and icons

--- modules/ui/general.js
+++ modules/ui/general.js
@@ -161,12 +161,14 @@ function showMapTooltip(point, e, i, g) {
   if (group === "terrain") return tip("Click to edit the Relief Icon");
 
   if (subgroup === "burgLabels" || subgroup === "burgIcons") {
-    const burg = +path[path.length - 10].dataset.id;
-    const b = pack.burgs[burg];
-    const population = si(b.population * populationRate * urbanization);
-    tip(`${b.name}. Population: ${population}. Click to edit`);
-    if (burgsOverview?.offsetParent) highlightEditorLine(burgsOverview, burg, 5000);
-    return;
+    const burgId = +path[path.length - 10].dataset.id;
+    if (burgId) {
+      const burg = pack.burgs[burgId];
+      const population = si(burg.population * populationRate * urbanization);
+      tip(`${burg.name}. Population: ${population}. Click to edit`);
+      if (burgsOverview?.offsetParent) highlightEditorLine(burgsOverview, burgId, 5000);
+      return;
+    }
   }
 
   if (group === "labels") return tip("Click to edit the Label");
