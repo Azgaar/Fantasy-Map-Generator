861b219e6e7c1811fc2b0514e5404235d700aab1
Azgaar <azgaar.fmg@yandex.com>
2024-09-21 14:23:18 +0200

    fix: don't hide armies layer

--- modules/io/load.js
+++ modules/io/load.js
@@ -459,7 +459,7 @@ async function parseLoadedData(data, mapVersion) {
 
     {
       // dynamically import and run auto-update script
-      const {resolveVersionConflicts} = await import("../dynamic/auto-update.js?v=1.104.5");
+      const {resolveVersionConflicts} = await import("../dynamic/auto-update.js?v=1.104.12");
       resolveVersionConflicts(mapVersion);
     }
 
