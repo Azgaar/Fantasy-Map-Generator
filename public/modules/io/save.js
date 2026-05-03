45d65733667711e67638211c0cd5e51b889752dc
Hiroshi Yamaguchi <h-yamaguchi@shinonomekazan.com>
2026-05-04 01:08:38 +0900

    fix: The space between the start and end points of the Bézier curve is filled in.

--- public/modules/io/save.js
+++ public/modules/io/save.js
@@ -86,6 +86,14 @@ function prepareMapData() {
   cloneEl.querySelector("#viewbox").removeAttribute("transform");
 
   cloneEl.querySelector("#ruler").innerHTML = ""; // always remove rulers
+  
+  cloneEl
+    .querySelectorAll("#routes > #roads path, #routes > #trails path, #routes > #searoutes path")
+    .forEach(path => path.setAttribute("fill", "none"));
+  
+  cloneEl
+    .querySelectorAll("#borders > #stateBorders path, #borders > #provinceBorders path")
+    .forEach(path => path.setAttribute("fill", "none"));
 
   const serializedSVG = new XMLSerializer().serializeToString(cloneEl);
 
