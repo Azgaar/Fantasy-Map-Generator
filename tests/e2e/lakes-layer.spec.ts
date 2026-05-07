2e5294011f60078d8783c8a03c1e576e20b60f9d
Norbert de Langen <ndelangen@me.com>
2026-05-08 00:20:52 +0200

    Discard changes to tests/e2e/lakes-layer.spec.ts

--- tests/e2e/lakes-layer.spec.ts
+++ tests/e2e/lakes-layer.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 test.describe("Lakes layer", () => {
   test.beforeEach(async ({ context, page }) => {
