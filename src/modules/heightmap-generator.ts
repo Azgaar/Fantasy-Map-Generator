012bf4db9c06e33c9e3bfdb5dc8480543fb34930
MrHaribo <jonas_biedermann@hotmail.com>
2026-06-01 14:10:37 +0200

    Decreasing width exponent with every iteration (#1463)

--- src/modules/heightmap-generator.ts
+++ src/modules/heightmap-generator.ts
@@ -419,7 +419,8 @@ class HeightmapModule {
     const step = 0.1 / desiredWidth;
 
     for (let i = 0; i < desiredWidth; i++) {
-      const exp = 0.9 - step * desiredWidth;
+      const remainingWidth = desiredWidth - i;
+      const exp = 0.9 - step * remainingWidth;
       range.forEach((r: number) => {
         this.grid.cells.c[r].forEach((e: number) => {
           if (used[e]) return;
