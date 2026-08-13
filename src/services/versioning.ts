755ac04379e908dd9d4ab56a6f6e859d913d9d60
Azgaar <azgaar.fmg@yandex.com>
2026-08-13 11:24:37 +0200

    chore: bump version to 1.141.3 fix: improve handling of custom anchors in meander function

--- src/services/versioning.ts
+++ src/services/versioning.ts
@@ -15,7 +15,7 @@
  * For the changes that may be interesting to end users, update the `latestPublicChanges` array below (new changes on top).
  */
 
-export const VERSION = "1.141.2";
+export const VERSION = "1.141.3";
 
 const latestPublicChanges = [
   "Configurable table columns",
